bc31f0cec6d0255ef4b15198635a3ddc43ffc52b
Matthias Vogel <m.vogel@andersundsehr.com>
2025-08-20 08:17:32 +0200

    🐛 fix wrong queue path

--- Classes/Factory/QueueTransportFactory.php
+++ Classes/Factory/QueueTransportFactory.php
@@ -11,18 +11,25 @@ use AUS\SentryAsync\Factory\EntryFactory;
 use AUS\SentryAsync\Queue\FileQueue;
 use AUS\SentryAsync\Transport\QueueTransport;
 use Sentry\Transport\TransportInterface;
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\PathUtility;
 
 class QueueTransportFactory
 {
     public function __invoke(): TransportInterface
     {
+        $directory = (string)($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['sentry_bridge']['directory'] ?? 'var/andersundsehr/sentry-async/');
+        if (!PathUtility::isAbsolutePath($directory)) {
+            $directory = Environment::getProjectPath() . DIRECTORY_SEPARATOR . $directory;
+        }
+
         return new QueueTransport(
             GeneralUtility::makeInstance(
                 FileQueue::class,
                 (int)($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['sentry_bridge']['limit'] ?? 100),
                 (bool)($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['sentry_bridge']['compress'] ?? false),
-                (string)($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['sentry_bridge']['directory'] ?? 'typo3temp/sentry-bridge/'),
+                $directory,
                 GeneralUtility::makeInstance(EntryFactory::class, Entry::class)
             ),
             GeneralUtility::makeInstance(EntryFactory::class, Entry::class)
